641a2addbe2282f649459472992f1570bc873c6d
Deybert Sanchez <deybert@hotmail.com>
2024-10-30 23:48:36 -0300

    Mejoras al Codigo

--- Clases/ParserData.py
+++ Clases/ParserData.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import json
+import geohash as geo
+import chardet
 
 class ParserData():
 
@@ -13,47 +15,102 @@ class ParserData():
         self.v_conexion = conexion
     
     #declaracion de construcctor vacio
-    #def __init__(self):
+    # def __init__(self):
     #    print("Iniciada la Clase Vacia")
 
     #funcion para leer y limpiar el archivo csv
     def readData(self,ruta,file):
-        #Unifica la ruta mas nombre del archivo
-        self.v_file = ruta+file      
-        
-        #Se lee el arvhivo y se guarda en un dataFrame
-        df = pd.read_csv(self.v_file)
-        
-        print("antes")
-        print(len(df))
-        #elimina filas con datos en nulo
-        df = df.dropna(axis=0)
+         #Unifica la ruta mas nombre del archivo
+        self.v_file = ruta+file  
 
-        #elomina columnas con datos en nulo
-        df = df.dropna(axis=1)
-
-        print("despues")
-        print(len(df))
+        #Diferencia los archivos ya q tienen diferentes codificaciones
+        if file == "bocas-de-subte.csv":
+            df = pd.read_csv(self.v_file, encoding="ISO-8859-1")       
+        else:
+            df = pd.read_csv(self.v_file)
         
-        # #Se eliminan las columnas Hora, CAntidad y Sentido
-        df.drop(["HORA","CANTIDAD","SENTIDO"],inplace=True,axis=1)
+            #elimina filas con datos en nulo
+            df = df.dropna(axis=0)
 
-        # #Se eliminan los duplicados
-        df = df.drop_duplicates()
-
-        # #Se cambian los tipos de datos de las columans latitud y longitud
-        df['LATITUD'] = df['LATITUD'].astype('str')
-        df['LONGITUD'] = df['LONGITUD'].astype('str')
-        
-        # #Para insertar una nueva columna
-        # #df.insert(0,"ID",list(range(1,(len(df)+1))))
-        
-        #logs(Propios)
-        print("Datos Tratados")
+            #elomina columnas con datos en nulo
+            df = df.dropna(axis=1)        
 
         #retornamos el dataFrame
         return df
 
+    def insertTablaUbicacion(self,i_arrayVehiculos,i_arraySubtes):
+        #Variables
+        sql_insert = """ insert into ubicacion(id_ubicacion,latitud,longitud,geohash) values(%s,%s,%s,%s) """
+        v_array = []
+
+        #Tratamos los datos de los movimientos de vehiculos y subtes para guardar las ubicaciones
+        df_ubicacion1 = i_arrayVehiculos[["LATITUD","LONGITUD","GEOHASH"]]
+        df_ubicacion2 = i_arraySubtes[["lat","long"]].assign(GEOHASH=None)
+        df_ubicacion2.columns = ["LATITUD","LONGITUD","GEOHASH"]
+        df_ubicacion = pd.concat([df_ubicacion1,df_ubicacion2])
+        df_ubicacion = df_ubicacion.drop_duplicates()
+        df_ubicacion.insert(0,"ID",list(range(1,(len(df_ubicacion)+1))))          
+        
+        #Se recorre el df y se pasa a un arreglo para insertarlo en la tabla
+        for i in range(len(df_ubicacion)):
+            v_array.append((int(df_ubicacion.iloc[i]['ID']),                            
+                            str(df_ubicacion.iloc[i]['LATITUD']),
+                            str(df_ubicacion.iloc[i]['LONGITUD']),
+                            df_ubicacion.iloc[i]['GEOHASH']
+                        ))
+
+        try:
+            self.v_conexion.execQueryArray(queryParams=sql_insert,paramsArray=v_array)
+            self.v_conexion.commit()
+
+            #Log
+            print("Se insertaron los Datos de Ubicacion Correctamente")
+            
+            #Retormamos el df
+            return df_ubicacion
+
+        except ValueError as err:
+            print("Este es el error: "+err)
+    
+    def insertTablaSubte(self,i_arraySubtes,i_arrayUbicacion):
+        #Variables
+        sql_insert = """ insert into subte(id_subte,id_ubicacion,linea,estacion,molinete) values(%s,%s,%s,%s,%s) """
+        v_array = []
+
+        #Tratamos los datos de los movimientos de vehiculos y subtes para guardar las ubicaciones
+        df_tlbSubte = i_arraySubtes[["lat","long","linea","estacion"]].assign(molinte = i_arraySubtes["dom_orig"].fillna(i_arraySubtes["calle"]))
+        df_tlbSubte = df_tlbSubte.drop_duplicates()
+        df_tlbSubte = df_tlbSubte.sort_values(by=["linea","estacion"])
+        df_tlbSubte.columns = ['LATITUD','LONGITUD','LINEA','ESTACION','MOLINETE']
+        df_tlbSubte = pd.merge(df_tlbSubte,i_arrayUbicacion,on=['LATITUD', 'LONGITUD'], how='inner')
+        df_tlbSubte.insert(0,"ID_SUB",list(range(1,(len(df_tlbSubte)+1))))
+        df_tlbSubte = df_tlbSubte[["ID_SUB","ID","LINEA","ESTACION","MOLINETE"]]        
+        
+        #Se recorre el df y se pasa a un arreglo para insertarlo en la tabla
+        for i in range(len(df_tlbSubte)):
+            v_array.append((int(df_tlbSubte.iloc[i]['ID_SUB']),                            
+                            int(df_tlbSubte.iloc[i]['ID']),
+                            df_tlbSubte.iloc[i]['LINEA'],
+                            df_tlbSubte.iloc[i]['ESTACION'],
+                            df_tlbSubte.iloc[i]['MOLINETE']
+                        ))
+
+        try:
+            self.v_conexion.execQueryArray(queryParams=sql_insert,paramsArray=v_array)
+            self.v_conexion.commit()
+
+            #Log
+            print("Se insertaron los Datos de subtes Correctamente")
+            
+            #Retormamos el df
+            return df_tlbSubte
+        
+        except ValueError as err:
+            print("Este es el error: "+err)   
+        
+    
+
+    #-------------------------PRUEBAS-----------------------------------#
     #funcion para insertar datos en la tabla geo_hash(un solo registro)
     def insertSimpleGeoHash(self):
         sql_insert = """ insert into geo_hash(id,geo_hash,latitud,longitud) values(%s,%s,%s,%s) """
@@ -74,7 +131,21 @@ class ParserData():
     def insertTableGeoHash(self,i_arrayParams):
         sql_insert = """ insert into geo_hash(id,geo_hash,latitud,longitud) values(%s,%s,%s,%s) """
         v_array = []
-               
+
+        # #Se eliminan las columnas Hora, CAntidad y Sentido
+        i_arrayParams.drop(["HORA","CANTIDAD","SENTIDO"],inplace=True,axis=1)
+
+        # #Se eliminan los duplicados
+        i_arrayParams = i_arrayParams.drop_duplicates()
+
+        #Se cambian los tipos de datos de las columans latitud y longitud
+        #i_arrayParams['LATITUD'] = i_arrayParams['LATITUD'].astype('str')
+        #i_arrayParams['LONGITUD'] = i_arrayParams['LONGITUD'].astype('str')
+        
+        #Otra forma de cambiar el tipo de dato pero igual da una advertencia
+        #i_arrayParams.loc[:,'LATITUD'] = i_arrayParams.loc[:,'LATITUD'].astype(str)
+        #i_arrayParams.loc[:,'LONGITUD'] = i_arrayParams.loc[:,'LONGITUD'].astype('str')
+
         #opcion 1 544ms
         #for datos in i_arrayParams.itertuples():
         #    i=i+1
@@ -94,8 +165,8 @@ class ParserData():
         for i in range(len(i_arrayParams)):
             v_array.append((i+1,
                             i_arrayParams.iloc[i]['CODIGO_LOCACION'],
-                            i_arrayParams.iloc[i]['LATITUD'],
-                            i_arrayParams.iloc[i]['LONGITUD']))
+                            str(i_arrayParams.iloc[i]['LATITUD']),
+                            str(i_arrayParams.iloc[i]['LONGITUD'])))
         
         #print(v_array)            
         try:
@@ -107,4 +178,5 @@ class ParserData():
         
         except ValueError as err:
             print("Este es el error: "+err)
-        
+    
+            
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from Clases.Connection import Connection
 from Clases.ParserData import ParserData
+import pandas as pd
 import json
 
 #Definir el entorno que desea inicializar desde el archivo de configuracion
 #Entorno = "LOCAL"
 Entorno = "DEV"
-v_df = None
-v_nameFile = None
 
 #Obtener las variables de acceso
 with open('./config.json','r') as file:
@@ -22,17 +21,32 @@ conexion = Connection(USER=config[Entorno]['USER'],
 parserData = ParserData(conexion=conexion)
 
 #Instanciamos la Clase ParserData sin conexion
-#parserData = ParserData()
+# parserData = ParserData()
 
-#Definimos el archivo que se va a leer
-#v_nameFile = "output_file-1.csv"
-v_nameFile = "dataset_flujo_vehicular_formateado_cleaned.csv"
+#Definimos los archivos que se van a leer
+v_nameFileSubte = "subte_flujo.csv"
+v_nameFileVehicular = "flujo_vehicular_dc.csv"
+v_nameFileBocaSubte = "bocas-de-subte.csv"
 
 #Guardamos el resultado del dataFrame en una variable
-v_df = parserData.readData(ruta=config[Entorno]['REP_LOCAL'],file=v_nameFile)
+df_subte = parserData.readData(ruta=config[Entorno]['REP_LOCAL'],file=v_nameFileSubte)
+#print("Datos Tratados Subte Cantidad "+str(len(df_subte)))
+#print(df_subte.head(5))
 
-print(v_df)
-#Guardamos los datos en una tabla
-parserData.insertTableGeoHash(i_arrayParams=v_df)
-#intento cerrar la conexion
+df_vehicular = parserData.readData(ruta=config[Entorno]['REP_LOCAL'],file=v_nameFileVehicular)
+#print("Datos Tratados Vehicular Cantidad "+str(len(df_vehicular)))
+#print(df_vehicular.head(5))
+
+df_bocaSubtes = parserData.readData(ruta=config[Entorno]['REP_LOCAL'],file=v_nameFileBocaSubte)
+#print("Datos Tratados Bocas Cantidad "+str(len(df_bocaSubtes)))
+#print(df_bocaSubtes.head(5))
+#-----------------------------------------------------
+
+#Guardamos los datos ubicaciones en la tabla
+df_RUbicacion = parserData.insertTablaUbicacion(i_arrayVehiculos=df_vehicular.copy(),i_arraySubtes=df_bocaSubtes.copy())
+
+#Guardamos los datos de las bocas de subte en la tabla
+df_RSubte = parserData.insertTablaSubte(i_arraySubtes=df_bocaSubtes.copy(),i_arrayUbicacion=df_RUbicacion.copy())
+
+#Se cierra la conexion a la BD
 conexion.connClose()
